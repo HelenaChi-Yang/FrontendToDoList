f414df7016fe46dfe0b0ccf1fa8cbece86431ea8
helenayang1 <helena.yang@xuenn.com>
2025-02-13 18:54:32 +0800

    TarotGame

---
+++ my-todo-app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>TarotGame</h1>
+    <TarotGame msg="Welcome to TarotGame"/>
+  </div>
+</template>
+
+<script>
+import TarotGame from './components/TarotGame.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TarotGame
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-todo-app/src/components/TarotGame.vue
@@ -0,0 +1,103 @@
+<template>
+  <div>
+    <!-- 開始遊戲按鈕 -->
+    <button v-if="!gameStarted" @click="startGame">開始遊戲</button>
+
+    <!-- 牌組區塊 -->
+    <div v-if="gameStarted" class="card-container">
+      <div
+        class="card"
+        v-for="(card, index) in cards"
+        :key="index"
+        :class="{ flipped: card.flip}"
+        @click="flipCard(index)"
+      >
+        <div class="card-inner">
+          <div class="card-front">
+            {{ card.name }}
+          </div>
+          <div class="card-back">
+            🔮
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      gameStarted: false,
+      cards: [
+        { name: "The Fool", flip: false},
+        { name: "The Magician", flip: false},
+        { name: "The High Priestess", flip: false}
+      ]
+    };
+  },
+  methods: {
+    startGame() {
+      this.gameStarted = true;
+    },
+    flipCard(index) {
+      this.cards[index].flip = true;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.card-container {
+  display: flex;
+  justify-content: center;
+  gap: 10px;
+  margin-top: 20px;
+}
+.card {
+  width: 100px;
+  height: 150px;
+  border: 2px solid black;
+  /* 3D 透視視角 */
+  perspective: 1000px;
+  /* 確保 .card-inner 使用 position: absolute; 時，它的定位參考點是 .card */
+  position: relative;
+}
+.card-inner {
+  width: 100%;
+  height: 100%;
+  /* 確保 .card-inner 使用 position: absolute; 時，它的定位參考點是 .card */
+  position: absolute;
+  /* 影響子元素保留 3D 效果 */
+  transform-style: preserve-3d;
+  /* 設定 0.6 秒的翻轉動畫 */
+  transition: transform 0.6s;
+}
+
+/* 當 .card 同時擁有 flipped 類別時，讓 .card-inner 整個旋轉 180 度*/
+.flipped.card .card-inner {
+  transform: rotateY(180deg);
+}
+
+.card-front, .card-back {
+  width: 100%;
+  height: 100%;
+  /* position: absolute; */
+  backface-visibility: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 18px;
+}
+
+.card-front {
+  background-color: gold;
+  /* 翻轉 .card-front 讓它起始時朝向背面 */
+  transform: rotateY(180deg);
+}
+
+.card-back {
+  background-color: rgba(46, 0, 153, 0.499);
+}
+</style>
